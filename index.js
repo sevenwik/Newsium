4c53c33e431f5901981e41c5e54b5c6815ca9f42
sevenwik <sathwikchepyala99@gmail.com>
2021-11-18 21:01:20 +0530

    Added more features

--- index.js
+++ index.js
@@ -29,5 +29,68 @@ getInfo.getArchiveInfo = ((year, month) => {
         }
     })
 })
+getInfo.getArticleInfo = ((query) => {
+    return new Promise((done, err) => {
+        if(typeof query === 'undefined') {
+            err('Provide query to fetch articles related to that')
+        } else {
+            let apiKey = 'ZnqEsIALud8ejdd5wcZWAZxdlpFjBhnT'
+            axios(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${apiKey}`,{
+                method: "GET"
+            })
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.data.response.docs
+                    } else {
+                        err(`Sorry, Bad response code : ` + response.status)
+                    }
+                })
+                .then(json => done(json))
+                .catch(error => console.error(`Sorry, Error: ` + error))
+        }
+    })
+})
+getInfo.getTopStories = ((topic) => {
+    return new Promise((done, err) => {
+        if(typeof topic === 'undefined') {
+            err('Provide a topic to fetch top stories related to that')
+        } else {
+            let apiKey = 'ZnqEsIALud8ejdd5wcZWAZxdlpFjBhnT'
+            axios(`https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=${apiKey}`,{
+                method: "GET"
+            })
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.data
+                    } else {
+                        err(`Sorry, Bad response code : ` + response.status)
+                    }
+                })
+                .then(json => done(json))
+                .catch(error => console.error(`Sorry topic not found: ` + error))
+        }
+    })
+})
+getInfo.getMostPopular = ((type,days) => {
+    return new Promise((done, err) => {
+        if(typeof type === 'undefined' && typeof days === 'undefined') {
+            err('Provide a topic to fetch top stories related to that')
+        } else {
+            let apiKey = 'ZnqEsIALud8ejdd5wcZWAZxdlpFjBhnT'
+            axios(`https://api.nytimes.com/svc/mostpopular/v2/${type}/${days}.json?api-key=${apiKey}`,{
+                method: "GET"
+            })
+                .then(response => {
+                    if (response.status === 200) {
+                        return response.data
+                    } else {
+                        err(`Sorry, Bad response code : ` + response.status)
+                    }
+                })
+                .then(json => done(json))
+                .catch(error => console.error(`Sorry : ` + error))
+        }
+    })
+})
 
 export default getInfo
--- npm_test.js
+++ npm_test.js
@@ -1,8 +1,30 @@
 import getInfo from "./index.js"
 
-getInfo.getArchiveInfo("2019","1").then(obj => { 
+/*getInfo.getArchiveInfo("2019","1").then(obj => { 
     console.log("Archives ") 
 	console.log(obj)
 }).catch(err => {
+	console.log(err)
+})
+
+getInfo.getArticleInfo("meta").then(obj => { 
+    console.log("Articles ") 
+	console.log(obj)
+}).catch(err => {
+	console.log(err)
+})
+getInfo.getTopStories("science").then(obj => { 
+    console.log("Top Stories") 
+	console.log(obj)
+}).catch(err => {
+	
+	console.log(err)
+})*/
+
+getInfo.getMostPopular("viewed","1").then(obj => { 
+    console.log("Most Popular") 
+	console.log(obj)
+}).catch(err => {
+	
 	console.log(err)
 })
